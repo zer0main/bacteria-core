c4a8ef270c1197dfc36b6551e970270c7a1f5ba9
Pavel Dolgov <pdolgov99@gmail.com>
2016-03-18 23:42:36 +0300

    Add file for constants
    Add constant DEFAULT_MASS.

---
+++ constants.hpp
@@ -0,0 +1,13 @@
+/*
+ * bacteria-core, core for cellular automaton
+ * Copyright (C) 2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef CONSTANTS_HPP_
+#define CONSTANTS_HPP_
+
+const int DEFAULT_MASS = 5;
+
+#endif
