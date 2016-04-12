c550ee032ec42a953fe6059757c0ae20eb2ac79a
Pavel Dolgov <pdolgov99@gmail.com>
2016-04-12 20:55:54 +0300

    Dummy Bytecode files

---
+++ src/interpreter/Bytecode.cpp
@@ -0,0 +1,8 @@
+/*
+ * bacteria-core, core for cellular automaton
+ * Copyright (C) 2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include "Bytecode.hpp"
---
+++ src/interpreter/Bytecode.hpp
@@ -0,0 +1,11 @@
+/*
+ * bacteria-core, core for cellular automaton
+ * Copyright (C) 2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef BYTECODE_HPP_
+#define BYTECODE_HPP_
+
+#endif
