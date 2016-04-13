4141720d9da21cc398a8cb4a3082757f6c148d1e
Pavel Dolgov <pdolgov99@gmail.com>
2016-04-13 23:43:13 +0300

    Add struct Token

--- src/interpreter/Bytecode.cpp
+++ src/interpreter/Bytecode.cpp
@@ -6,3 +6,20 @@
  */
 
 #include "Bytecode.hpp"
+
+namespace Implementation {
+
+Token::Token(
+    Type type,
+    int parameter,
+    bool spec,
+    int function_id
+)
+    : type(type)
+    , parameter(parameter)
+    , spec(spec)
+    , function_id(function_id)
+{
+}
+
+}
--- src/interpreter/Bytecode.hpp
+++ src/interpreter/Bytecode.hpp
@@ -8,4 +8,30 @@
 #ifndef BYTECODE_HPP_
 #define BYTECODE_HPP_
 
+
+namespace Implementation {
+
+enum Type {
+    FUNCTION,
+    PARAMETER,
+    SPECIFICATION,
+    DELIMITER
+};
+
+struct Token {
+    Token(
+        Type type,
+        int parameter = -1,
+        bool spec = false,
+        int function_id = -1
+    );
+
+    Type type;
+    int parameter;
+    int function_id;
+    bool spec;
+};
+
+}
+
 #endif
