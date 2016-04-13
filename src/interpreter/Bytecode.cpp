8f20a0bd0741f082f7136f4d5f9db1e67d9bec01
Pavel Dolgov <pdolgov99@gmail.com>
2016-04-14 00:05:52 +0300

    Description of language syntax
    Add some static arrays.

--- src/interpreter/Bytecode.cpp
+++ src/interpreter/Bytecode.cpp
@@ -9,6 +9,36 @@
 
 namespace Implementation {
 
+// These two static arrays describe language: all possible
+// functions ans specifications.
+
+static const char* const specifications_registry[] = {
+    "r"
+};
+
+static const char* const functions_registry[] = {
+    "eat", "go", "clon", "str", "left", "right", "back", "turn",
+    "jg", "jl", "j", "je", "jfe", "jw", "jfw", "jb", "jfb"
+};
+
+// These four static arrays contain IDs of functions from
+// functions_registry.
+
+static const int two_parameter_functions[] = {
+    8, 9
+};
+
+static const int one_parameter_functions[] = {
+    0, 1, 3, 4, 5, 10, 11, 12, 13, 14, 15, 16
+};
+
+static const int spec_functions[] = {
+    0, 1, 7
+};
+
+static const int non_argument_functions[] = {
+    0, 1, 2, 3, 4, 5, 6
+};
 
 static void split(
     const std::string& str,
