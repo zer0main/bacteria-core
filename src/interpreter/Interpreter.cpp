97c80bb767a0943d9eb51b334c906570161bed94
Pavel Dolgov <pdolgov99@gmail.com>
2016-04-24 15:30:14 +0300

    Implement Interpreter::makeMove()

--- src/interpreter/Interpreter.cpp
+++ src/interpreter/Interpreter.cpp
@@ -39,6 +39,20 @@ void Interpreter::makeMove_impl(
     Abstract::Changer& changer,
     Abstract::State* st
 ) const {
+    int bacteria = changer.getBacteriaNumber();
+    int team = changer.getTeam();
+    for (int b = 0; b < bacteria; b++) {
+        while (!changer.endOfMove(b)) {
+            int instruction_number = changer.getInstruction(b);
+            PackedInstruction pi =
+                bytecode_[team]->getInstruction(instruction_number);
+            int func_id = pi.function_id;
+            Abstract::Params params(pi.p1, pi.p2, pi.spec);
+            ChangerMethod func = changer_functions[func_id];
+            (changer.*func)(&params, b);
+        }
+    }
+    changer.clearAfterMove();
 }
 
 Abstract::State* Interpreter::createState_impl() const {
