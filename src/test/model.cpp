4064a1996624490414c010bd9a485f16918d3574
Pavel Dolgov <pdolgov99@gmail.com>
2016-06-26 16:42:48 +0300

    Add module for Model tests
    Add first test (kill_test)

---
+++ src/test/model.cpp
@@ -0,0 +1,34 @@
+/*
+ * bacteria-core, core for cellular automaton
+ * Copyright (C) 2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <boost/test/unit_test.hpp>
+
+#include "Model.hpp"
+
+BOOST_AUTO_TEST_CASE (kill_test) {
+    Implementation::Model* model =
+        Abstract::makeModel<Implementation::Model>(
+            MIN_WIDTH,
+            MIN_HEIGHT,
+            0,
+            1
+        );
+    Abstract::Point coordinates(0, 0);
+    model->createNewByCoordinates(
+        coordinates,
+        DEFAULT_MASS,
+        0,
+        0,
+        0
+    );
+    Abstract::CellState state = model->cellState(coordinates);
+    BOOST_REQUIRE(state == Abstract::BACTERIUM);
+    model->killByCoordinates(coordinates);
+    state = model->cellState(coordinates);
+    BOOST_REQUIRE(state == Abstract::EMPTY);
+    delete model;
+}
