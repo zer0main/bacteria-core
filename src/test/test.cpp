d0b9f40cd62be1bdfbbdf63babf0061dc0ba6688
Pavel Dolgov <pdolgov99@gmail.com>
2016-06-26 16:40:37 +0300

    Add test.cpp (boost-test main file)

---
+++ src/test/test.cpp
@@ -0,0 +1,9 @@
+/*
+ * bacteria-core, core for cellular automaton
+ * Copyright (C) 2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#define BOOST_TEST_MAIN
+#include <boost/test/included/unit_test.hpp>
