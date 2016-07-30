6a1457a5a5beacae206d6277a1a3eb749f6b3276
Pavel Dolgov <pdolgov99@gmail.com>
2016-07-30 23:07:44 +0300

    Integration tests (run_tests.sh): multiple scripts
    Use numbers [0-9] for naming different .bact files in
    single test.
    Rename eat.bact and go.bact files.

--- integration_tests/run_tests.sh
+++ integration_tests/run_tests.sh
@@ -8,9 +8,9 @@ input_files=$(ls tests/*.yaml | grep -v '.expected.yaml$')
 
 for f in $input_files; do
     name=$(basename "$f" .yaml)
-    scripts=$(ls "tests/${name}.bact")
     observed="tests/${name}.observed.yaml"
-    valgrind $integration_test "$f" "$scripts" "$observed"
+    # .bact files are scripts with bacterium intelligence
+    valgrind $integration_test "$f" "tests/${name}"[0-9]".bact" "$observed"
 done
 
 observed_files=$(ls tests/*.observed.yaml)
