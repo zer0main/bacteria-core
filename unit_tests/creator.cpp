4baf11f3e79de0384525f5bcdb75f209bda17afd
Pavel Dolgov <pdolgov99@gmail.com>
2016-08-20 17:02:38 +0300

    Unit tests (creator): add create_special_model_test

--- unit_tests/creator.cpp
+++ unit_tests/creator.cpp
@@ -22,6 +22,28 @@ static void baseModelChecks(
     BOOST_REQUIRE(model->getBacteriaNumber(0) == bacteria_number);
 }
 
+BOOST_AUTO_TEST_CASE (create_special_model_test) {
+    Implementation::Unit unit(
+        Abstract::Point(0, 0),
+        DEFAULT_MASS,
+        0,
+        0,
+        0
+    );
+    Units units(1, unit);
+    ModelPtr model = Creator::createSpecialModel(
+        MIN_WIDTH,
+        MIN_HEIGHT,
+        1,
+        units
+    );
+    baseModelChecks(model, MIN_WIDTH, MIN_HEIGHT, 1);
+    BOOST_REQUIRE(model->getCoordinates(0, 0) == unit.coordinates);
+    BOOST_REQUIRE(model->getMass(0, 0) == unit.mass);
+    BOOST_REQUIRE(model->getDirection(0, 0) == unit.direction);
+    BOOST_REQUIRE(model->getInstruction(0, 0) == unit.instruction);
+}
+
 BOOST_AUTO_TEST_CASE (create_model_test) {
     ModelPtr model = Creator::createModel(
         MIN_WIDTH,
