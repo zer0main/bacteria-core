c24e4409274a6245401a072b8e792c61d13ee0c8
Pavel Dolgov <pdolgov99@gmail.com>
2016-08-20 17:00:46 +0300

    Unit tests (creator): static baseModelChecks()
    Use it in create_model_test.

--- unit_tests/creator.cpp
+++ unit_tests/creator.cpp
@@ -11,6 +11,17 @@
 #include "CoreGlobals.hpp"
 #include "Creator.hpp"
 
+static void baseModelChecks(
+    ModelPtr model,
+    int width,
+    int height,
+    int bacteria_number
+) {
+    BOOST_REQUIRE(model->getWidth() == width);
+    BOOST_REQUIRE(model->getHeight() == height);
+    BOOST_REQUIRE(model->getBacteriaNumber(0) == bacteria_number);
+}
+
 BOOST_AUTO_TEST_CASE (create_model_test) {
     ModelPtr model = Creator::createModel(
         MIN_WIDTH,
@@ -18,7 +29,5 @@ BOOST_AUTO_TEST_CASE (create_model_test) {
         1,
         1
     );
-    BOOST_REQUIRE(model->getWidth() == MIN_WIDTH);
-    BOOST_REQUIRE(model->getHeight() == MIN_HEIGHT);
-    BOOST_REQUIRE(model->getBacteriaNumber(0) == 1);
+    baseModelChecks(model, MIN_WIDTH, MIN_HEIGHT, 1);
 }
