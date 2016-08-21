d8a06cfc0a31b3cd3802de3d01d993c67720b767
Pavel Dolgov <pdolgov99@gmail.com>
2016-08-21 14:00:12 +0300

    Unit tests (changer): add get_bacteria_number_test

---
+++ unit_tests/changer.cpp
@@ -0,0 +1,21 @@
+/*
+ * bacteria-core, core for cellular automaton
+ * Copyright (C) 2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <boost/test/unit_test.hpp>
+
+#include "TestFunctions.hpp"
+
+#include "CoreConstants.hpp"
+#include "CoreGlobals.hpp"
+#include "Model.hpp"
+#include "Changer.hpp"
+
+BOOST_AUTO_TEST_CASE (get_bacteria_number_test) {
+    ModelPtr model(createBaseModel(1, 1));
+    Implementation::Changer changer(model, 0, 0, 0);
+    BOOST_REQUIRE(changer.getBacteriaNumber() == 1);
+}
