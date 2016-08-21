843d3b86a5a6ad3a3e82d7abb27ae840667419a4
Pavel Dolgov <pdolgov99@gmail.com>
2016-08-21 16:04:37 +0300

    Unit tests (changer): add changer_get_instruction_test

--- unit_tests/changer.cpp
+++ unit_tests/changer.cpp
@@ -25,3 +25,25 @@ BOOST_AUTO_TEST_CASE (get_team_test) {
     Implementation::Changer changer(model, 0, 0, 0);
     BOOST_REQUIRE(changer.getTeam() == 0);
 }
+
+BOOST_AUTO_TEST_CASE (changer_get_instruction_test) {
+    ModelPtr model(createBaseModel(1, 1));
+    // total number of instructions is 2
+    Implementation::Changer changer(model, 0, 0, 2);
+    // initial state
+    BOOST_REQUIRE(changer.getInstruction(0) == 0);
+    Abstract::Params params(-1, -1, false);
+    changer.eat(&params, 0);
+    // after execution of the first command
+    BOOST_REQUIRE(changer.getInstruction(0) == 1);
+    int commands = 5;
+    params = Abstract::Params(commands, -1, false);
+    for (int i = 0; i < commands; i++) {
+        // it must execute the same command `commands` times
+        BOOST_REQUIRE(changer.getInstruction(0) == 1);
+        changer.clearBeforeMove();
+        changer.eat(&params, 0);
+    }
+    // cycle: after execution of all commands it returns to initial state
+    BOOST_REQUIRE(changer.getInstruction(0) == 0);
+}
